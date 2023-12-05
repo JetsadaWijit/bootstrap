9d340f3670319def6c3e8b588e8602f871d02c8f
owen <56456776+JetsadaWijit@users.noreply.github.com>
2023-12-05 16:58:10 +0700

    Update header.js

--- docs/script/header.js
+++ docs/script/header.js
@@ -44,7 +44,7 @@ header.appendChild(nav);
 document.body.appendChild(header);
 
 // Create a style element for CSS
-/*const style = document.createElement('style');
+const style = document.createElement('style');
 style.innerHTML = `
 header {
     margin-left: 77vh;
@@ -102,4 +102,4 @@ nav > * {
 ;
 
 // Append style to the head
-document.head.appendChild(style);*/
+document.head.appendChild(style);
